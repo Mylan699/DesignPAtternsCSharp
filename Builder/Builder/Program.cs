a7da4198ad5373a87d38abcc26787844cac277e9
Mylan699 <mylan.mahieddine@ecole-isitech.fr>
2024-12-30 15:05:19 +0100

    Vendeur + Immatriculation

--- Builder/Builder/Program.cs
+++ Builder/Builder/Program.cs
@@ -1,4 +1,5 @@
 using Builder;
+using System;
 
 namespace Builder
 {
@@ -6,6 +7,9 @@ namespace Builder
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Entrez le nom du vendeur :");
+            string vendeurNom = Console.ReadLine();
+
             Console.WriteLine("Choisissez le format de liasse (html/pdf) :");
             string choix = Console.ReadLine()?.ToLower();
 
@@ -28,8 +32,24 @@ namespace Builder
             Console.WriteLine("Entrez les informations du véhicule :");
             string informationsVehicule = Console.ReadLine();
 
+            // Générer une immatriculation française fictive
+            string immatriculation = GenererImmatriculation();
+
             Liasse liasse = directeur.Construit(client, informationsVehicule);
+            liasse.AjouteDocument($"Vendeur : {vendeurNom}");
+            liasse.AjouteDocument($"Immatriculation générée : {immatriculation}");
             liasse.Imprime();
+
+            Console.WriteLine("Liasse générée avec succès !");
+        }
+
+        static string GenererImmatriculation()
+        {
+            Random random = new Random();
+            string lettres1 = $"{(char)random.Next('A', 'Z' + 1)}{(char)random.Next('A', 'Z' + 1)}";
+            string chiffres = random.Next(100, 999).ToString();
+            string lettres2 = $"{(char)random.Next('A', 'Z' + 1)}{(char)random.Next('A', 'Z' + 1)}";
+            return $"{lettres1}-{chiffres}-{lettres2}";
         }
     }
 }
