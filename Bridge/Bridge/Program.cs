59dfeb784af7729a83c924757dedf18ac72682b5
Mylan699 <mylan.mahieddine@ecole-isitech.fr>
2024-12-31 14:55:55 +0100

    Bridge

---
+++ Bridge/Bridge/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Bridge;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Utiliser le formulaire France avec implémentation HTML
+        ImplFormulaireImmatriculation htmlImpl = new ImplFormulaireHtml();
+        FormulaireImmatriculation formulaireFrance = new FormulaireImmatriculationFrance(htmlImpl);
+        formulaireFrance.Afficher();
+
+        Console.WriteLine();
+
+        // Utiliser le formulaire Luxembourg avec implémentation Applet
+        ImplFormulaireImmatriculation appletImpl = new ImplFormulaireApplet();
+        FormulaireImmatriculation formulaireLux = new FormulaireImmatriculationLuxembourg(appletImpl);
+        formulaireLux.Afficher();
+    }
+}
--- Bridge/Bridge/obj/Debug/net8.0/Bridge.AssemblyInfo.cs
+++ Bridge/Bridge/obj/Debug/net8.0/Bridge.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Builder")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Bridge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Builder")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Builder")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dae5f6e4ba53f564da64cd9a29bfcd950f336ea3")]
+[assembly: System.Reflection.AssemblyProductAttribute("Bridge")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Bridge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
